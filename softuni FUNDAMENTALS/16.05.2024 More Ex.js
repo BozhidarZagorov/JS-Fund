0bb712e01eafe518f358e5497df370fec3292e91
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2024-05-18 15:13:16 +0300

    Update 16.05.2024 More Ex.js
    commented

--- softuni FUNDAMENTALS/16.05.2024 More Ex.js
+++ softuni FUNDAMENTALS/16.05.2024 More Ex.js
@@ -10,33 +10,33 @@
 // sortNum(-2,1,3)
 // sortNum(0,0,2)
 
-function numToWord(input) {
-    let a=input%10
-    console.log(a);
-    if (a === 0) {
-        console.log(`zero`);
-    }else if (a === 1) {
-        console.log(`one`);
-    }else if (a === 2) {
-        console.log(`two`);
-    }else if (a === 3) {
-        console.log(`three`);
-    }else if (a === 4) {
-        console.log(`four`);
-    }else if (a === 5) {
-        console.log(`five`);
-    }else if (a === 6) {
-        console.log(`six`);
-    }else if (a === 7) {
-        console.log(`seven`);
-    }else if (a === 8) {
-        console.log(`eight`);
-    }else if (a === 9) {
-        console.log(`nine`);
-    }
-}
+//function numToWord(input) {
+//    let a=input%10
+//    console.log(a);
+//    if (a === 0) {
+//        console.log(`zero`);
+//    }else if (a === 1) {
+//       console.log(`one`);
+//    }else if (a === 2) {
+//        console.log(`two`);
+//    }else if (a === 3) {
+//        console.log(`three`);
+//    }else if (a === 4) {
+//        console.log(`four`);
+//    }else if (a === 5) {
+//        console.log(`five`);
+//    }else if (a === 6) {
+//        console.log(`six`);
+//    }else if (a === 7) {
+//        console.log(`seven`);
+//   }else if (a === 8) {
+//        console.log(`eight`);
+//    }else if (a === 9) {
+//        console.log(`nine`);
+//    }
+//}
 
-numToWord(1643)
-numToWord(512)
-numToWord(1)
-numToWord(20)
+//numToWord(1643)
+//numToWord(512)
+//numToWord(1)
+//numToWord(20)
