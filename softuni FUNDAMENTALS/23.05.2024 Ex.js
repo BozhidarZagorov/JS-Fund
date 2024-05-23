8881425a3335b9e705637583fae5acf14f2bc271
Bozhidar <bozhidarzagorov@gmail.com>
2024-05-23 19:43:56 +0300

    23.05.2024 ex added and problems 1-7

---
+++ softuni FUNDAMENTALS/23.05.2024 Ex.js
@@ -0,0 +1,52 @@
+// function sumDigits(num) {
+//     let sum = 0
+//     // let numStr = num.toString()
+//     let numStr = num + ''  //stringova go
+
+//     for (let i = 0; i < numStr.length; i++) {
+//         let curDigit=Number(numStr[i])
+//         sum+=curDigit
+//     }
+//     console.log(sum);
+// }
+// sumDigits(255)
+// sumDigits(245678)
+
+// function charToStr(a,b,c) {
+//     console.log(a+b+c);
+// }
+// charToStr(`a`,`b`,`c`)
+// charToStr('%','2','o')
+
+// function town(town,population,area) {
+//     console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+// }
+// town('Sofia',1286383,492)
+
+// function mToKm(metres) {
+//     console.log((metres/1000).toFixed(2));
+// }
+// mToKm(1852)
+// mToKm(798)
+
+// function poundsToDollars(pounds) {
+//     console.log((pounds*1.31).toFixed(3));
+// }
+// poundsToDollars(80)
+
+// function reversedChars(a,b,c) {
+//     console.log(`${c} ${b} ${a}`);
+// }
+// reversedChars('A','B','C')
+
+// function lowerOrUpper(ch) {
+//     if (ch === ch.toUpperCase()) {
+//         console.log(`upper-case`);
+//     }else console.log(`lower-case`);
+// }
+// lowerOrUpper(`L`)
+// lowerOrUpper('f')
+
+
+// more ex 
+// console.log(parseInt(binaryNum, 2)); На 5та зад. от more exercise решението
